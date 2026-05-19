f8f8d5ab29f18ab9a8d5b4c8ae3d734d7e87d676
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-05-19 18:24:56 +0300

    feat: link User with Account

--- Projects/Mini_ATM/ATM_v3/models/user.py
+++ Projects/Mini_ATM/ATM_v3/models/user.py
@@ -1,7 +1,8 @@
 class User:
-    def __init__(self, username, pin):
+    def __init__(self, username, pin, account):
         self.username = username
         self.__pin = pin
+        self.account = account
 
     def check_pin(self, entered_pin):
         return entered_pin == self.__pin
