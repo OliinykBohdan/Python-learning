bccfa042331353224c6a1f05a5983d7f782b52d5
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-05-24 11:39:58 +0300

    refactor: reorganize project structure

--- Projects/Telegram_Bots/01_First_simple_bot/bot.py
+++ Projects/Telegram_Bots/01_First_simple_bot/bot.py
@@ -2,7 +2,7 @@ from os import getenv
 import asyncio
 from aiogram import Bot, Dispatcher
 from dotenv import load_dotenv
-from Projects.Telegram_Bots.handlers.routes import router
+from handlers import router
 
 load_dotenv()
 TOKEN = getenv('BOT_TOKEN')
