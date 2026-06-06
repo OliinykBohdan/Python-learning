a4648fbacc719ed7d72d71ecfdb0532948c8557a
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-06-06 13:01:33 +0300

    refactor: organize task files and output formatting

--- Basics/06_tuple.py
+++ Basics/06_tuple.py
@@ -1,6 +1,17 @@
+# Task 1
+
 numbers = (1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 1, 1)
 
 print(numbers)
 
 print(numbers.index(2))
 print(numbers.count(1))
+print('Result: done')
+
+# Task 2
+
+date = (6, 'June', 2026)
+
+number, month, year = date
+
+print(f'It\'s {number} {month}, {year}.')
