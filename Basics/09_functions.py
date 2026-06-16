a0ccf5dc5132446f4a269e9a298092c15b2be755
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-06-16 11:44:31 +0300

    feat: add tasks 3 and 4

--- Basics/09_functions.py
+++ Basics/09_functions.py
@@ -23,3 +23,35 @@ multiply(15)
 multiply()
 
 print('Result: done')
+
+# Task 3
+
+print('-' * 10, 'Task 3:', sep='\n')
+
+raw_url = 'example.com'
+
+def func(url):
+    if 'www' not in url:
+        url = 'https://www.' + url
+
+    elif 'https://' not in url:
+        url = 'https://' + url
+
+    return url
+
+print('Result:', func(raw_url))
+
+# Task 4
+
+print('-' * 10, 'Task 4:', sep='\n')
+
+def percent_of(value, part, /):
+    if value <= 0 or part < 0:
+        return False
+
+    precent = part / value * 100
+    return str(round(precent, 2)) + ' %'
+
+result = percent_of(100,20)
+
+print('Result:', result)
