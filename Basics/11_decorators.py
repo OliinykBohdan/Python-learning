8a79b472ab7112252c5faa7b3153621d7d582952
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-06-20 18:31:57 +0300

    style: add spacing between function and code

--- Basics/11_decorators.py
+++ Basics/11_decorators.py
@@ -1,5 +1,6 @@
 # Task 1
 
+
 def decorator (func):
     def inner ():
         print('start')
@@ -7,19 +8,23 @@ def decorator (func):
         print('end')
     return inner
 
+
 @decorator
 def say_name1 ():
     print('Bohdan')
 
+
 @decorator
 def say_name2 ():
     print('Vika')
+
 
 say_name1()
 
 say_name2()
 
 # Task 2
+
 
 def decorator_1(func):
     def inner():
@@ -28,6 +33,7 @@ def decorator_1(func):
         print(10 * '~')
     return inner
 
+
 def decorator_2(func):
     def inner():
         print(10 * '#')
@@ -35,14 +41,17 @@ def decorator_2(func):
         print(10 * '#')
     return inner
 
+
 @decorator_2
 @decorator_1
 def func_1():
     print('Main content')
 
+
 func_1()
 
 # Task 3
+
 
 def decorator_maker():
     def decorator_3(func):
@@ -53,13 +62,16 @@ def decorator_maker():
         return inner
     return decorator_3
 
+
 @decorator_maker()
 def func_1(name):
     print(f'Main content {name}')
 
+
 func_1('123')
 
 # Task 4
+
 
 def repeat(num = 3):
     def decorator_3(func):
@@ -69,6 +81,7 @@ def repeat(num = 3):
         return inner
     return decorator_3
 
+
 @repeat(5)
 def func_1(name):
     print(f'Main content {name}')
@@ -77,6 +90,7 @@ def func_1(name):
 @repeat()
 def func_2(name):
     print(f'Main content {name}')
+
 
 func_1('123')
 func_2('54754')
