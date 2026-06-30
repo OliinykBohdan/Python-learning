99fa08a48cc6cbb5d828debed21d03fcf6f913a7
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-06-30 19:22:16 +0300

    style: add spacing between function and code

--- Basics/10_closures.py
+++ Basics/10_closures.py
@@ -2,10 +2,12 @@
 
 print('-' * 10, 'Task 1:', sep='\n')
 
+
 def say_name(name):
     def say_hello():
         print(f'Hello {name}!')
     return say_hello
+
 
 f = say_name('Bohdan')
 f()
@@ -14,10 +16,12 @@ f()
 
 print('-' * 10, 'Task 2:', sep='\n')
 
+
 def strip_spring (strip_chars = ' '):
     def do_strip (striping):
         return striping.strip(strip_chars)
     return do_strip
+
 
 strip1 = strip_spring()
 strip2 = strip_spring(' !?,.')
