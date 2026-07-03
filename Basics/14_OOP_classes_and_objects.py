9e137db9de4c0952344f1f2764e1434111942428
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-07-03 17:15:22 +0300

    feat: add task 3

--- Basics/14_OOP_classes_and_objects.py
+++ Basics/14_OOP_classes_and_objects.py
@@ -1,3 +1,5 @@
+import os #import used in Task 3
+
 # Task 1
 
 print('-' * 10, 'Task 1:', sep='\n')
@@ -58,3 +60,35 @@ x.plus()
 
 print('Result:', y)
 print('Result:', x)
+
+# Task 3
+
+print('-' * 10, 'Task 3:', sep='\n')
+
+
+class Path:
+    def __init__(self, path=None):
+        self.CWD = os.getcwd()
+        self.current = path or os.path.dirname(__file__)
+
+    def parent(self):
+        self.current = os.path.dirname(self.current)
+        return self
+
+    def get_parent(self):
+        return os.path.dirname(self.current)
+
+    def add_dir(self, dirname):
+        self.current = os.path.join(self.current, dirname)
+        return self
+
+    def get_path(self):
+        return self.current
+
+    def __str__(self):
+        return str(self.current)
+
+
+path = Path().parent().add_dir('Test')
+
+print('Directory:', path.current)
