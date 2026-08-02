00553b60746e0aab5fa4e8e2f86513990af733ce
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-08-02 12:46:12 +0300

    feat: add task 2

--- Basics/08_dict.py
+++ Basics/08_dict.py
@@ -27,3 +27,35 @@ print('After merging dictionaries:', user)
 # Safely access a non-existent key with default value
 hobby = user.get('hobby1', 0)
 print('Hobby value:', hobby)
+
+# Task 2
+
+print('-' * 10, 'Task 2:', sep='\n')
+
+temps_celsius = [
+    ['06:00', -1.5],
+    ['07:00', 0.2],
+    ['08:00', 2.8],
+    ['09:00', 5.6],
+    ['10:00', 8.4],
+    ['11:00', 11.2],
+    ['12:00', 14.7],
+    ['13:00', 17.0],
+    ['14:00', 18.3],
+    ['15:00', 19.1],
+    ['16:00', 19.4],
+    ['17:00', 18.8],
+    ['18:00', 17.0],
+    ['19:00', 14.5],
+    ['20:00', 11.2],
+    ['21:00', 7.8],
+    ['22:00', 4.0]
+]
+
+temps_d = dict(temps_celsius)
+
+temps_d['14:00'] = 14.7
+temps_d['23:00'] = 2.7
+del temps_d['10:00']
+
+print(temps_d)
