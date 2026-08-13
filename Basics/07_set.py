79c68c2faf33ef458557c3d78fabbfda0529c4b8
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-08-13 14:31:33 +0300

    feat: add union(), issubset(), issuperset() examples

--- Basics/07_set.py
+++ Basics/07_set.py
@@ -1,7 +1,10 @@
-# Methods covered: add(), remove(), intersection(), symmetric_difference(), difference()
+# Methods covered:
+# add(), remove(), intersection(), symmetric_difference(), difference(), union(),
+# issubset(), issuperset()
 
 numbers1 = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 numbers2 = {4, 3433, 23, 32, 1, 34, 8, 6, 2, 3}
+numbers3 = {1, 3, 7}
 
 numbers1.add(11)
 numbers2.remove(4)
@@ -20,3 +23,15 @@ print('symmetric_difference():', result, sep='\n')
 # Elements only in numbers1
 result = numbers1.difference(numbers2)
 print('difference():', result, sep='\n')
+
+# Combines sets into one
+result = numbers1.union(numbers2)
+print('union():', result, sep='\n')
+
+# Checks whether all elements of the set are contained in the other set
+result = numbers3.issubset(numbers1)
+print('issubset():', result, sep='\n')
+
+# Shows the same thing as issubset(), just the other way around
+result = numbers1.issuperset(numbers3)
+print('issuperset():', result, sep='\n')
