05a2bf009c34b680c9ce52dc6db84c5c62a67c74
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-08-13 13:32:49 +0300

    style: improve output messages

--- Basics/07_set.py
+++ Basics/07_set.py
@@ -6,17 +6,17 @@ numbers2 = {4, 3433, 23, 32, 1, 34, 8, 6, 2, 3}
 numbers1.add(11)
 numbers2.remove(4)
 
-print(numbers1)
-print(numbers2)
+print('add():', numbers1, sep='\n')
+print('remove():', numbers2, sep='\n')
 
 # Elements present in both sets
 result = numbers1.intersection(numbers2)
-print(result)
+print('intersection():', result, sep='\n')
 
 # Elements unique to each set
 result = numbers1.symmetric_difference(numbers2)
-print(result)
+print('symmetric_difference():', result, sep='\n')
 
 # Elements only in numbers1
 result = numbers1.difference(numbers2)
-print(result)
+print('difference():', result, sep='\n')
