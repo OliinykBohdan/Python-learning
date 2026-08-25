d6e78ce69ef57fa6d31fe92804f6838a37f9731e
OliinykBohdan <spirit.ghost.2020@gmail.com>
2026-08-25 15:15:57 +0300

    feat: add task 2

--- Tasks/15_Error_handling.py
+++ Tasks/15_Error_handling.py
@@ -20,3 +20,31 @@ def convert_temperature(temp_celsius: int |float, /):
 
 
 print('Temperature in degrees Fahrenheit:', convert_temperature(1))
+
+# Task 2: Functions + error handling
+#
+# Description:
+# Create a function divide_numbers(a, b) that returns the result of dividing a by b.
+#
+# The function should:
+# - accept only int or float values;
+# - reject boolean values;
+# - raise TypeError if one of the arguments is not a number;
+# - raise ZeroDivisionError if b == 0;
+# - return the division result if all conditions are valid.
+
+print('-' * 10, 'Task 2:', sep='\n')
+
+
+def divide_numbers(a, b):
+    if not isinstance(a, int |float) or not isinstance(b, int |float)\
+            or isinstance(a, bool) or isinstance(b, bool):
+        raise TypeError('not a number entered')
+
+    if b == 0:
+        raise ZeroDivisionError('division by zero')
+
+    return round(a / b, 2)
+
+
+print('Result:', divide_numbers(121, 3))
